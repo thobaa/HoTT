a24158e0009842a92d20772bb794bac23de68c94
mikeshulman <viritrilbia@gmail.com>
2011-11-21 11:51:39 -0800

    Forgot to add UsefulEquivalences in Homotopy.v

--- Coq/Homotopy.v
+++ Coq/Homotopy.v
@@ -1,3 +1,3 @@
-Require Export Paths Fibrations Contractible Equivalences FiberEquivalences FiberSequences.
+Require Export Paths Fibrations Contractible Equivalences UsefulEquivalences FiberEquivalences FiberSequences.
 Require Export Funext Univalence UnivalenceAxiom.
 Require Export HLevel.
