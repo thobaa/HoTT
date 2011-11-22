d521177e3b586ddac810f3b5280ea6993fd70a24
mikeshulman <viritrilbia@gmail.com>
2011-11-22 07:35:13 -0800

    Added descent along coproducts.

--- Coq/Subcategories/LocalSubfibration.v
+++ Coq/Subcategories/LocalSubfibration.v
@@ -6,7 +6,12 @@ Require Import Homotopy ReflectiveSubfibration IsInhab.
    is *local*, in the sense that the property of belonging to it
    descends along effective epimorphisms (defined as morphisms whose
    [is_inhab]-image is their whole codomain, i.e. morphisms each of
-   whose homotopy fibers is inhabited). *)
+   whose homotopy fibers is inhabited).  Thus, it is a stack for the 
+   "regular toplogy".
+
+   Similarly, and even more trivially, it is a stack for the extensive
+   topology, and even the "indexed-extensive topology".
+   *)
 
 Section LocalSubfibration.
 
@@ -15,6 +20,8 @@ Section LocalSubfibration.
   Existing Instance is_rsf.
 
   Hint Resolve reflect_in_rsc unit_in_rsc prod_in_rsc path_in_rsc exp_in_rsc.
+
+  Section EffEpi.
 
   Hypothesis A B : Type.
   Hypothesis f : A -> B.
@@ -31,4 +38,41 @@ Section LocalSubfibration.
     apply f_ee.
   Defined.
 
+  End EffEpi.
+
+  Section BinaryCoproduct.
+
+    Hypothesis A B : Type.
+    Hypothesis P : A+B -> Type.
+    Hypothesis Pa_in_rsc : forall a, in_rsc (P (inl _ a)).
+    Hypothesis Pb_in_rsc : forall b, in_rsc (P (inr _ b)).
+
+    Theorem rsc_is_extensive_binary (ab : A+B) : in_rsc (P ab).
+    Proof.
+      destruct ab; auto.
+    Defined.
+
+  End BinaryCoproduct.
+
+  Theorem rsc_is_extensive_nullary (P : Empty_set -> Type) (x:Empty_set) : in_rsc (P x).
+  Proof.
+    induction x.
+  Defined.
+
+  Section IndexedCoproduct.
+
+    Hypothesis A : Type.
+    Hypothesis B : A -> Type.
+    Hypothesis P : sigT B -> Type.
+    Hypothesis Pb_in_rsc : forall a b, in_rsc (P (a ; b)).
+
+    Theorem rsc_is_extensive_indexed (ab:sigT B) : in_rsc (P ab).
+    Proof.
+      destruct ab.
+      auto.
+    Defined.
+
+  End IndexedCoproduct.
+
 End LocalSubfibration.
+
