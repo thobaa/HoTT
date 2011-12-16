88c7db5fd16f0d1b722cebdf52722c059f6eecb6
mikeshulman <viritrilbia@gmail.com>
2011-12-15 17:15:43 -0800

    More funext equivalences.

--- Coq/FunextEquivalences.v
+++ Coq/FunextEquivalences.v
@@ -44,6 +44,16 @@ Proof.
   apply inverse_is_retraction.
 Defined.
 
+Lemma postcomp_equiv_dep A P Q (g : forall a:A, P a <~> Q a) :
+  (forall a, P a) <~> (forall a, Q a).
+Proof.
+  exists (fun f a => g a (f a)).
+  apply @hequiv_is_equiv with (g := fun k a => (g a ^-1) (k a));
+    intros k; apply funext_dep; intros a; unfold compose; simpl.
+  apply inverse_is_section.
+  apply inverse_is_retraction.
+Defined.
+
 (** Cartesian products have the correct universal property. *)
 
 Lemma prod_equiv A B T :
@@ -57,3 +67,32 @@ Proof.
   intros [f g]; apply prod_path; apply funext; intros t; auto.
 Defined.
 
+(** Given an iterated fibration, to give a section of the composite
+   fibration is equivalent to giving a section of the first fibration and
+   a section over that of the second.  *)
+
+Lemma section_total_equiv A (P : A -> Type) (Q : forall a, P a -> Type) :
+  (forall a, sigT (Q a)) <~> {s : forall a, P a & forall a, Q a (s a) }.
+Proof.
+  exists (fun f => (existT (fun s => forall a, Q a (s a))
+    (fun a => pr1 (f a)) (fun a => pr2 (f a)))).
+  apply hequiv_is_equiv with
+    (g := fun sr:{s : forall a, P a & forall a, Q a (s a) } =>
+      let (s,r) := sr in (fun a => existT (Q a) (s a) (r a))).
+  intros [s r].
+  set (p := funext_dep (f := eta_dep s) (g := s) (fun a => idpath (s a))).
+  apply total_path with p; simpl.
+  apply funext_dep; intros a.
+  apply (concat (trans_map p
+    (fun s' (r': forall a, Q a (s' a)) => r' a) (eta_dep r))).
+  path_via (transport (happly_dep p a) (eta_dep r a)).
+  unfold happly_dep.
+  apply @map_trans with (f := fun h : forall a' : A, P a' => h a).
+  path_via (transport (idpath (s a)) (eta_dep r a)).
+  apply happly, map.
+  apply funext_dep_compute with
+    (f := eta_dep s) (g := s) (p := fun a' : A => idpath (s a')).
+  intros f.
+  apply funext_dep; intros a.
+  destruct (f a); auto.
+Defined.
