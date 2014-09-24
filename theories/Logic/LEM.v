66a2cf9da0bf277fbd6f6ed0619a40a890604cea
Mike Shulman <viritrilbia@gmail.com>
2014-09-24 11:17:58 -0700

    indent section

--- theories/Logic/LEM.v
+++ theories/Logic/LEM.v
@@ -12,62 +12,62 @@ Local Open Scope path_scope.
 (** * Excluded middle *)
 
 Section LEM.
-Context {fs : Funext}.
+  Context {fs : Funext}.
 
-(** If we end up needing this universe-polymorphically, we could change it to an axiom depending on a dummy typeclass like [Funext] and [Univalence].  Its name ends with an underscore to indicate that it is parametrized by the logic (as a "subscript").*)
-Class LEM_ (log : Logic) :=
-  lem : forall P, (P \/ (~ P)).
+  (** If we end up needing this universe-polymorphically, we could change it to an axiom depending on a dummy typeclass like [Funext] and [Univalence].  Its name ends with an underscore to indicate that it is parametrized by the logic (as a "subscript").*)
+  Class LEM_ (log : Logic) :=
+    lem : forall P, (P \/ (~ P)).
 
-(** If proving LEM, we may as well assume that [P] is a generic proposition. *)
-Definition easyLEM (log : Logic)
-: (forall P, IsGenericProp P -> (P \/ (~ P))) -> LEM_ log.
-Proof.
-  intros H P.
-  refine (hor_elim  _ _ (H (@O log P) _)).
-  - apply O_rect; try exact _.
-    apply hor_inl.
-  - intros nop.
+  (** If proving LEM, we may as well assume that [P] is a generic proposition. *)
+  Definition easyLEM (log : Logic)
+  : (forall P, IsGenericProp P -> (P \/ (~ P))) -> LEM_ log.
+  Proof.
+    intros H P.
+    refine (hor_elim  _ _ (H (@O log P) _)).
+    - apply O_rect; try exact _.
+      apply hor_inl.
+    - intros nop.
+      apply hor_inr.
+      intros p.
+      exact (nop (O_unit P p)).
+  Defined.
+
+  (** The double negation of LEM always holds. *)
+  Theorem notnot_LEM (log : Logic) (P : Type)
+  : ~ ~ (P \/ (~ P)).
+  Proof.
+    intros K.
+    refine (K _).
     apply hor_inr.
     intros p.
-    exact (nop (O_unit P p)).
-Defined.
+    refine (K _).
+    exact (hor_inl p).
+  Defined.
 
-(** The double negation of LEM always holds. *)
-Theorem notnot_LEM (log : Logic) (P : Type)
-: ~ ~ (P \/ (~ P)).
-Proof.
-  intros K.
-  refine (K _).
-  apply hor_inr.
-  intros p.
-  refine (K _).
-  exact (hor_inl p).
-Defined.
+  (** As a corollary, LEM itself holds in double-negation logic.  This proof would be substantially more complicated if our definition of [notnot_logic] didn't record the fact that [Empty] is modal, since in that case [LEM_ notnot_logic] would involve a [~~Empty] where [notnot_LEM PAT_logic] has an [Empty]. *)
+  Definition LEM_notnot : LEM_ notnot_logic :=
+    fun P => notnot_LEM PAT_logic P.
 
-(** As a corollary, LEM itself holds in double-negation logic.  This proof would be substantially more complicated if our definition of [notnot_logic] didn't record the fact that [Empty] is modal, since in that case [LEM_ notnot_logic] would involve a [~~Empty] where [notnot_LEM PAT_logic] has an [Empty]. *)
-Definition LEM_notnot : LEM_ notnot_logic :=
-  fun P => notnot_LEM PAT_logic P.
+  (** LEM is equivalent to the law of double negation, in any logic.  Note that in the law of double negation, we *do* have to assume that [P] is a generic proposition. *)
+  Theorem double_negation {log : Logic} {lem : @LEM_ log} P `{IsGenericProp P}
+  : (~ ~ P) -> P.
+  Proof.
+    pose (pnp := lem P).
+    intros nnp.
+    refine (hor_elim _ _ pnp).
+    - exact idmap.
+    - intros np.
+      refine (hfalse_elim _ _).
+      exact (nnp np).
+  Defined.
 
-(** LEM is equivalent to the law of double negation, in any logic.  Note that in the law of double negation, we *do* have to assume that [P] is a generic proposition. *)
-Theorem double_negation {log : Logic} {lem : @LEM_ log} P `{IsGenericProp P}
-: (~ ~ P) -> P.
-Proof.
-  pose (pnp := lem P).
-  intros nnp.
-  refine (hor_elim _ _ pnp).
-  - exact idmap.
-  - intros np.
-    refine (hfalse_elim _ _).
-    exact (nnp np).
-Defined.
-
-Theorem lem_from_double_negation {log : Logic}
- : (forall P, IsGenericProp P -> (~ ~ P) -> P) -> LEM_ log.
-Proof.
-  intros H; apply easyLEM; intros P ?.
-  apply H; try exact _.
-  apply notnot_LEM.
-Defined.
+  Theorem lem_from_double_negation {log : Logic}
+  : (forall P, IsGenericProp P -> (~ ~ P) -> P) -> LEM_ log.
+  Proof.
+    intros H; apply easyLEM; intros P ?.
+                                    apply H; try exact _.
+    apply notnot_LEM.
+  Defined.
 
 End LEM.
 
@@ -79,7 +79,7 @@ Section LEMisprop.
 
   Local Definition DN P {P_inO : inO P}
   : ~~P -> P
-  := @double_negation log _ P _.
+    := @double_negation log _ P _.
 
   Local Definition nnobool : ~~(O Bool)
     := (fun nb => nb (O_unit Bool true)).
@@ -97,12 +97,12 @@ Section LEMisprop.
   Defined.
 
   Local Definition Onegb_b : O_functor negb b = b.
-    pose (p := ap10 (apD DN (path_universe (O_functor negb))) _).
-    rewrite transport_arrow in p.
-    refine (_ @ ap10 p nnobool).
-    rewrite transport_arrow, transport_path_universe.
-    apply ap; unfold b.
-    apply ap011; apply allpath_hprop.
+  pose (p := ap10 (apD DN (path_universe (O_functor negb))) _).
+  rewrite transport_arrow in p.
+  refine (_ @ ap10 p nnobool).
+  rewrite transport_arrow, transport_path_universe.
+  apply ap; unfold b.
+  apply ap011; apply allpath_hprop.
   Qed.
 
   Local Definition Otrue_is_Ofalse :
@@ -120,7 +120,7 @@ Section LEMisprop.
       + rewrite <- p.
         apply Onegb_b.
   Qed.
-    
+  
   Theorem allO_hprop_lem P `{IsGenericProp P} : IsHProp P.
   Proof.
     apply hprop_allpath; intros x y.
@@ -139,7 +139,7 @@ This is Corollary 3.2.7. *)
 Theorem not_LEMoo {ua : Univalence} : ~ LEM_ oo.
 Proof.
   intros ?.
-  pose (H := allO_hprop_lem oo Bool).
+         pose (H := allO_hprop_lem oo Bool).
   apply true_ne_false, allpath_hprop.
 Defined.
 
